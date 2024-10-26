67e61919a00ec8cdeb6bb379103f5370636e1bd3
gizemyuksel <gizem.682@windowslive.com>
2024-10-26 20:48:42 +0300

    first commit

---
+++ LetsTestXcodeCloudService/LetsTestXcodeCloudServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsTestXcodeCloudServiceApp.swift
+//  LetsTestXcodeCloudService
+//
+//  Created by Gizem Yüksel on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LetsTestXcodeCloudServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
